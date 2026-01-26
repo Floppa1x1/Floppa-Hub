f992099cf6d862b1fe8c37ee420e0d11738d5f79
Floppa1x1 <floppa4x4@gmail.com>
2026-01-26 23:01:07 +0100

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -3,7 +3,7 @@ local placeId = game.PlaceId
 
 local scripts = {
     [111530421351096] = "https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/lua.lua", -- You VS Homer
-    [17625359962] = "https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/Rivals.lua",
+    [17625359962] = "https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/Rivals.lua", -- Rivals
 }
 
 local scriptUrl = scripts[placeId]
