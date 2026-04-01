c50587a5afe6c1394d0bac7b5613947796839e1d
Floppa1x1 <floppa4x4@gmail.com>
2026-04-02 00:06:04 +0200

    Add conditional loading for additional scripts

--- lua.lua
+++ lua.lua
@@ -1,5 +1,7 @@
 -- Hello, you shouldn't be here.
-
 if game.PlaceId == 124473577469410 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/Be_a_Lucky_Block.lua"))()
+end
+if game.PlaceId == 70845479499574 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/bbn.lua"))()
 end
