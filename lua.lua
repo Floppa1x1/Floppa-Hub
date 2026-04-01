decb81cd7aef29410a48a7b78ed84d7a974ffc6f
Floppa1x1 <floppa4x4@gmail.com>
2026-04-01 02:13:15 +0200

    Refactor game PlaceId checks in lua.lua
    Removed conditional loading for specific game PlaceIds.

--- lua.lua
+++ lua.lua
@@ -1,10 +1,5 @@
 -- Hello, you shouldn't be here.
-if game.PlaceId == 130594398886540 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/Garden_Horizons.lua"))()
-end
-if game.PlaceId == 111530421351096 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/yvsh.lua"))()
-end
+
 if game.PlaceId == 124473577469410 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/Be_a_Lucky_Block.lua"))()
 end
