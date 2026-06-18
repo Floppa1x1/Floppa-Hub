605ae5c24f97c4c817bb3536ad3df8cebe856cc5
Floppa1x1 <floppa4x4@gmail.com>
2026-06-18 16:59:12 +0200

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,4 +1,4 @@
 -- Hello, you shouldn't be here.
 if game.PlaceId == 124473577469410 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/Be_a_Lucky_Block.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/gag2.lua"))()
 end
