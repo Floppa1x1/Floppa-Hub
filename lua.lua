48d288acfc8054760c2ef8e72936cbbb73b75f7a
Floppa1x1 <floppa4x4@gmail.com>
2026-07-11 19:53:46 +0200

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,6 +1,9 @@
 if game.PlaceId == 79268393072444 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/lemons.lua"))()
 end
+if game.PlaceId == 79268393072444 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/lemons.lua"))()
+end
 if game.PlaceId == 142823291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Floppa1x1/Floppa-Hub/refs/heads/main/mm2.lua"))()
 end
